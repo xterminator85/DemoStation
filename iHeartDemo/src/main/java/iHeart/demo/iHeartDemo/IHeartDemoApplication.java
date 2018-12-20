fdbb646a3988c5f0de79dd326e7bbfee9f13d3b6
Kenny Rajiah <kenny.rajiah@ibm.com@kennys-mbp.austin.ibm.com>
2018-12-20 13:32:08 -0800

    first commit

---
+++ iHeartDemo/src/main/java/iHeart/demo/iHeartDemo/IHeartDemoApplication.java
@@ -0,0 +1,14 @@
+package iHeart.demo.iHeartDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHeartDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHeartDemoApplication.class, args);
+	}
+
+}
+
