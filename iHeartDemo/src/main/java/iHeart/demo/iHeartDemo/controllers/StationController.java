abe042aed0019036c40ad22f49960ca5101e97f7
Kenny Rajiah <kenny.rajiah@ibm.com@kennys-mbp.ibmmobiledemo.com>
2018-12-21 07:45:32 -0800

    added swagger

--- iHeartDemo/src/main/java/iHeart/demo/iHeartDemo/controllers/StationController.java
+++ iHeartDemo/src/main/java/iHeart/demo/iHeartDemo/controllers/StationController.java
@@ -1,9 +1,11 @@
 package iHeart.demo.iHeartDemo.controllers;
 
+import io.swagger.annotations.Api;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
